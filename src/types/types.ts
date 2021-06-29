9ec1029a10a73780d03aea34b859305d581eaf39
MuradBuyukasik <10285764+MuradBuyukasik@users.noreply.github.com>
2021-06-30 01:08:39 +0200

    removed user type

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,12 +3,3 @@ import { Prisma } from '@prisma/client'
 
 const entry = Prisma.validator<Prisma.EntryArgs>()({})
 export type Entry = Prisma.EntryGetPayload<typeof entry>
-
-export type User = {
-	name: string,
-	image: string,
-	accessToken?: string,
-	refreshToken?: string,
-	id: string,
-	discriminator: string
-}
