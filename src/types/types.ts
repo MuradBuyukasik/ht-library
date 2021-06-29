1b956e644b9a85fe9631601384f6913a3e9873e7
MuradBuyukasik <10285764+MuradBuyukasik@users.noreply.github.com>
2021-06-29 23:30:27 +0200

    added user type

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,12 @@ import { Prisma } from '@prisma/client'
 
 const entry = Prisma.validator<Prisma.EntryArgs>()({})
 export type Entry = Prisma.EntryGetPayload<typeof entry>
+
+export type User = {
+	name: string,
+	image: string,
+	accessToken?: string,
+	refreshToken?: string,
+	id: string,
+	discriminator: string
+}
