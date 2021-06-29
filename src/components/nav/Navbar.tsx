c6d69b991a6ff4969b2c14b3bac65ceb8c9cd9e0
MuradBuyukasik <10285764+MuradBuyukasik@users.noreply.github.com>
2021-06-30 00:03:15 +0200

    created simple navbar

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { User } from '@/types/types'
+import styles from './Navbar.module.css'
+import Link from 'next/link'
+import { useSession, signIn } from 'next-auth/client'
+import NameTag from '@/components/user/NameTag'
+
+interface Props {
+	user?: User
+}
+
+const Navbar: React.FC<Props> = ({ user }) => {
+	const [session, loading] = useSession()
+
+	const profileAction = () => {
+		if (session) {
+			return <NameTag name={session.user.name} imageUrl={session.user.image} />
+		} else {
+			return <button onClick={() => signIn()}>Login with Discord</button>
+		}
+	}
+
+	return (
+		<div className={styles.container}>
+			<div className={`grow-1 ${styles['space-between']}`}>
+				<Link href="/">
+					<a>
+						Home
+				</a>
+				</Link>
+				<Link href="#">
+					<a>
+						Post entry
+				</a>
+				</Link>
+			</div>
+			{profileAction()}
+		</div>
+	)
+}
+
+export default Navbar
